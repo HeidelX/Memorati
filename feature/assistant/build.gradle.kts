4cc00301be847b97494a4e043eb26fbeb574bbbc
Mohamed <Mohamed.Metwalli@sap.com>
2023-05-21 12:27:02 +0200

    Modules

---
+++ feature/assistant/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("memorati.android.feature")
+    id("memorati.android.library.compose")
+    id("memorati.android.library.jacoco")
+}
+
+android {
+    namespace = "com.memorati.feature.assistant"
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.accompanist.permissions)
+}
---
+++ feature/favourites/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("memorati.android.feature")
+    id("memorati.android.library.compose")
+    id("memorati.android.library.jacoco")
+}
+
+android {
+    namespace = "com.memorati.feature.favourites"
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.accompanist.permissions)
+}
