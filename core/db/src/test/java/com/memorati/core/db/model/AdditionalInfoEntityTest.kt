8eb1d1f478bf8cce2a6f15a72ead341c00260b64
Mohamed <Mohamed.Metwalli@sap.com>
2023-06-19 16:42:04 +0200

    Update AdditionalInfoEntityTest.kt

--- core/db/src/test/java/com/memorati/core/db/model/AdditionalInfoEntityTest.kt
+++ core/db/src/test/java/com/memorati/core/db/model/AdditionalInfoEntityTest.kt
@@ -8,49 +8,36 @@ import kotlin.test.assertEquals
 
 class AdditionalInfoEntityTest {
 
+    private val infoString1 = """
+                {"difficulty":1.0,"consecutiveCorrectCount":0,"memoryStrength":1.0}
+    """.trimIndent()
+    private val infoString2 = """
+                {"difficulty":2.0,"consecutiveCorrectCount":0,"memoryStrength":1.0}
+    """.trimIndent()
+
     @Test
     fun `Encode AdditionalInfoEntity to string`() {
         assertEquals(
-            """
-            {"difficulty":2.0,"consecutiveCorrectCount":0,"memoryStrength":1.0}
-            """.trimIndent(),
-            Json.encodeToString(
-                AdditionalInfoEntity(
-                    difficulty = 2.0,
-                ),
-            ),
+            infoString2,
+            Json.encodeToString(AdditionalInfoEntity(difficulty = 2.0)),
         )
 
         assertEquals(
-            """
-            {"difficulty":1.0,"consecutiveCorrectCount":0,"memoryStrength":1.0}
-            """.trimIndent(),
-            Json.encodeToString(
-                AdditionalInfoEntity(),
-            ),
+            infoString1,
+            Json.encodeToString(AdditionalInfoEntity()),
         )
     }
 
     @Test
     fun `Decode string to AdditionalInfoEntity `() {
         assertEquals(
-            AdditionalInfoEntity(
-                difficulty = 2.0,
-            ),
-            Json.decodeFromString(
-                """
-            {"difficulty":2.0,"consecutiveCorrectCount":0,"memoryStrength":1.0}
-                """.trimIndent(),
-            ),
+            AdditionalInfoEntity(difficulty = 2.0),
+            Json.decodeFromString(infoString1),
         )
 
         assertEquals(
             AdditionalInfoEntity(),
-            Json.decodeFromString(
-                """
-            {"difficulty":1.0,"consecutiveCorrectCount":0,"memoryStrength":1.0}
-                """.trimIndent(),
-            ),
+            Json.decodeFromString(infoString1),
         )
     }
 }
