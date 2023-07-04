501dcfee4095100e32a829ae8c43098e72a1365d
Mohamed <Mohamed.Metwalli@sap.com>
2023-07-04 22:35:01 +0200

    Chunks

--- core/db/src/main/java/com/memorati/core/file/DataTransferSource.kt
+++ core/db/src/main/java/com/memorati/core/file/DataTransferSource.kt
@@ -8,16 +8,15 @@ import com.memorati.core.db.model.FlashcardEntity
 import com.memorati.core.db.model.TransferableCard
 import kotlinx.coroutines.flow.first
 import kotlinx.datetime.Clock
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
 class DataTransferSource @Inject constructor(
-    private val flashcardsDao: FlashcardsDao,
+    private val dao: FlashcardsDao,
 ) {
     suspend fun flashcardsJson(): String {
-        val transferableCards = flashcardsDao.allFlashcard().first().map { flashcardEntity ->
+        val transferableCards = dao.allFlashcard().first().map { flashcardEntity ->
             TransferableCard(
                 idiom = flashcardEntity.front,
                 description = flashcardEntity.back,
@@ -31,21 +30,26 @@ class DataTransferSource @Inject constructor(
 
     suspend fun import(json: String) {
         val dataTransfer = Json.decodeFromString<DataTransfer>(json)
-        dataTransfer.cards.forEach { card ->
-            if (flashcardsDao.findBy(card.idiom.trim()).isEmpty()) {
-                flashcardsDao.insert(
-                    FlashcardEntity(
-                        flashcardId = 0,
-                        createdAt = Clock.System.now(),
-                        lastReviewAt = Clock.System.now(),
-                        nextReviewAt = Clock.System.now().plus(REVIEW_DURATION),
-                        front = card.idiom.trim(),
-                        back = card.description.trim(),
-                        favoured = false,
-                        additionalInfoEntity = AdditionalInfoEntity(),
-                    ),
-                )
-            } // else scape repeated card
-        }
+        dataTransfer.cards
+            .chunked(30)
+            .forEachIndexed { index, chunk ->
+                chunk.forEach { card ->
+                    if (dao.findBy(card.idiom.trim()).isEmpty()) {
+                        dao.insert(
+                            FlashcardEntity(
+                                flashcardId = 0,
+                                createdAt = Clock.System.now(),
+                                lastReviewAt = Clock.System.now(),
+                                nextReviewAt = Clock.System.now()
+                                    .plus(REVIEW_DURATION * (index + 1)),
+                                front = card.idiom.trim(),
+                                back = card.description.trim(),
+                                favoured = false,
+                                additionalInfoEntity = AdditionalInfoEntity(),
+                            ),
+                        )
+                    } // else scape repeated card
+                }
+            }
     }
 }
