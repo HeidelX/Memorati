5cb50aeb664a9713589d4182d343b9593edcc45f
Mohamed <Mohamed.Metwalli@sap.com>
2023-07-04 09:33:07 +0200

    Structure Notifications Settings

--- feature/settings/src/main/java/com/memorati/feature/settings/NotificationsSettings.kt
+++ feature/settings/src/main/java/com/memorati/feature/settings/NotificationsSettings.kt
@@ -1,128 +1,135 @@
 package com.memorati.feature.settings
 
+import MemoratiIcons
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.memorati.core.model.UserData
+import com.memorati.feature.settings.TimePickerRequest.END
+import com.memorati.feature.settings.TimePickerRequest.START
 import kotlinx.datetime.toDateTimePeriod
 
 @Composable
 internal fun NotificationsSettings(
+    modifier: Modifier = Modifier,
     userData: UserData,
     timeRequest: (TimePickerRequest) -> Unit,
     showDurationPicker: (Boolean) -> Unit,
 ) {
     Column(
-        modifier = Modifier.padding(10.dp),
+        modifier = modifier.padding(vertical = 10.dp),
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                imageVector = MemoratiIcons.Snooze,
-                contentDescription = stringResource(R.string.quiet_time),
-            )
-            Spacer(modifier = Modifier.width(5.dp))
-            Text(
-                text = stringResource(R.string.quiet_time),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier
-                .height(48.dp)
-                .clickable { timeRequest(TimePickerRequest.START) },
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = stringResource(R.string.start_time),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-            Spacer(modifier = Modifier.weight(1.0f))
-            Text(
-                text = userData.startTime.toString(),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-        }
-
-        Row(
-            modifier = Modifier
-                .height(48.dp)
-                .clickable { timeRequest(TimePickerRequest.END) },
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = stringResource(R.string.end_time),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-
-            Spacer(modifier = Modifier.weight(1.0f))
-
-            Text(
-                text = userData.endTime.toString(),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-        }
+        SectionTile(
+            imageVector = MemoratiIcons.Snooze,
+            label = stringResource(R.string.quiet_time),
+        )
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                imageVector = MemoratiIcons.Timelapse,
-                contentDescription = stringResource(R.string.reminder_interval),
-            )
-            Spacer(modifier = Modifier.width(5.dp))
-            Text(
-                text = stringResource(R.string.reminder_interval),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-        }
+        TimeRow(
+            label = stringResource(R.string.start_time),
+            time = userData.startTime.toString(),
+            onClick = { timeRequest(START) },
+        )
+
+        TimeRow(
+            label = stringResource(R.string.end_time),
+            time = userData.endTime.toString(),
+            onClick = { timeRequest(END) },
+        )
 
         Spacer(modifier = Modifier.height(10.dp))
+
+        SectionTile(
+            imageVector = MemoratiIcons.Timelapse,
+            label = stringResource(R.string.reminder_interval),
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
 
         Text(
+            modifier = Modifier.padding(horizontal = 10.dp),
             text = stringResource(id = R.string.interval_description),
             style = MaterialTheme.typography.titleSmall,
         )
 
-        Spacer(modifier = Modifier.height(5.dp))
+        TimeRow(
+            label = stringResource(R.string.interval),
+            time = stringResource(
+                R.string.interval_format,
+                userData.reminderInterval.toDateTimePeriod().hours,
+                userData.reminderInterval.toDateTimePeriod().minutes,
+            ),
+            onClick = { showDurationPicker(true) },
+        )
+    }
+}
 
-        Row(
+@Composable
+private fun SectionTile(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    label: String,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = label,
+        )
+
+        Text(
+            modifier = Modifier.padding(start = 10.dp),
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+@Composable
+private fun TimeRow(
+    modifier: Modifier = Modifier,
+    label: String,
+    time: String,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .height(48.dp)
+            .clip(CircleShape)
+            .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
             modifier = Modifier
-                .height(48.dp)
-                .clickable { showDurationPicker(true) },
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(
-                text = stringResource(R.string.interval),
-                style = MaterialTheme.typography.bodyMedium,
-            )
+                .padding(start = 10.dp)
+                .weight(1.0f),
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+        )
 
-            Spacer(modifier = Modifier.weight(1.0f))
-
-            Text(
-                text = stringResource(
-                    R.string.interval_format,
-                    userData.reminderInterval.toDateTimePeriod().hours,
-                    userData.reminderInterval.toDateTimePeriod().minutes,
-                ),
-                style = MaterialTheme.typography.bodyMedium,
-            )
-        }
+        Text(
+            modifier = Modifier.padding(end = 10.dp),
+            text = time,
+            style = MaterialTheme.typography.bodyMedium,
+        )
     }
 }
 
