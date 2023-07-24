2666b161652f35447e569e08986fd52fae2838c2
Mohamed <Mohamed.Metwalli@sap.com>
2023-07-24 22:46:26 +0200

    Update icons style

--- core/design/src/main/java/com/memorati/core/design/icon/MemoratiIcons.kt
+++ core/design/src/main/java/com/memorati/core/design/icon/MemoratiIcons.kt
@@ -1,48 +1,48 @@
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.Alarm
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.ArrowCircleDown
-import androidx.compose.material.icons.rounded.ArrowCircleUp
-import androidx.compose.material.icons.rounded.Assistant
-import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Delete
-import androidx.compose.material.icons.rounded.Done
-import androidx.compose.material.icons.rounded.Edit
-import androidx.compose.material.icons.rounded.KeyboardArrowDown
-import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material.icons.rounded.Share
-import androidx.compose.material.icons.rounded.Snooze
-import androidx.compose.material.icons.rounded.Star
-import androidx.compose.material.icons.rounded.StarBorder
-import androidx.compose.material.icons.rounded.Storage
-import androidx.compose.material.icons.rounded.Timelapse
-import androidx.compose.material.icons.rounded.ViewAgenda
-import androidx.compose.material.icons.rounded.VolumeUp
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Alarm
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.ArrowCircleDown
+import androidx.compose.material.icons.outlined.ArrowCircleUp
+import androidx.compose.material.icons.outlined.Assistant
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material.icons.outlined.Snooze
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.outlined.StarBorder
+import androidx.compose.material.icons.outlined.Storage
+import androidx.compose.material.icons.outlined.Timelapse
+import androidx.compose.material.icons.outlined.ViewAgenda
+import androidx.compose.material.icons.outlined.VolumeUp
 
 object MemoratiIcons {
-    val Settings = Icons.Rounded.Settings
-    val Edit = Icons.Rounded.Edit
-    val MoreVert = Icons.Rounded.MoreVert
-    val Delete = Icons.Rounded.Delete
-    val Cards = Icons.Rounded.ViewAgenda
-    val Add = Icons.Rounded.Add
-    val Search = Icons.Rounded.Search
-    val Favourites = Icons.Rounded.Star
-    val FavouritesBorder = Icons.Rounded.StarBorder
-    val Assistant = Icons.Rounded.Assistant
-    val ArrowBack = Icons.Rounded.ArrowBack
-    val Done = Icons.Rounded.Done
-    val Close = Icons.Rounded.Close
-    val Export = Icons.Rounded.ArrowCircleUp
-    val Import = Icons.Rounded.ArrowCircleDown
-    val Storage = Icons.Rounded.Storage
-    val Notifications = Icons.Rounded.Alarm
-    val Snooze = Icons.Rounded.Snooze
-    val Timelapse = Icons.Rounded.Timelapse
-    val Speak = Icons.Rounded.VolumeUp
-    val Share = Icons.Rounded.Share
-    val ArrowDown = Icons.Rounded.KeyboardArrowDown
+    val Settings = Icons.Outlined.Settings
+    val Edit = Icons.Outlined.Edit
+    val MoreVert = Icons.Outlined.MoreVert
+    val Delete = Icons.Outlined.Delete
+    val Cards = Icons.Outlined.ViewAgenda
+    val Add = Icons.Outlined.Add
+    val Search = Icons.Outlined.Search
+    val Favourites = Icons.Outlined.Star
+    val FavouritesBorder = Icons.Outlined.StarBorder
+    val Assistant = Icons.Outlined.Assistant
+    val ArrowBack = Icons.Outlined.ArrowBack
+    val Done = Icons.Outlined.Done
+    val Close = Icons.Outlined.Close
+    val Export = Icons.Outlined.ArrowCircleUp
+    val Import = Icons.Outlined.ArrowCircleDown
+    val Storage = Icons.Outlined.Storage
+    val Notifications = Icons.Outlined.Alarm
+    val Snooze = Icons.Outlined.Snooze
+    val Timelapse = Icons.Outlined.Timelapse
+    val Speak = Icons.Outlined.VolumeUp
+    val Share = Icons.Outlined.Share
+    val ArrowDown = Icons.Outlined.KeyboardArrowDown
 }
