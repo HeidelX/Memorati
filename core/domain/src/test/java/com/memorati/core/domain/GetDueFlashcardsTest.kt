606d664b971e15d1f21d9d8696a70e005cf96243
Mohamed <Mohamed.Metwalli@sap.com>
2023-07-26 23:50:18 +0200

    Update GetDueFlashcardsTest.kt

--- core/domain/src/test/java/com/memorati/core/domain/GetDueFlashcardsTest.kt
+++ core/domain/src/test/java/com/memorati/core/domain/GetDueFlashcardsTest.kt
@@ -1,17 +1,24 @@
 package com.memorati.core.domain
 
-import com.memorati.core.testing.repository.FakeFlashcardsRepository
+import com.memorati.core.common.time.AppTime
+import com.memorati.core.testing.repository.TestFlashcardsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import org.junit.Test
+import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
+import kotlin.time.Duration.Companion.hours
 
 class GetDueFlashcardsTest {
 
     @Test
     fun `Cards with low 'consecutiveCorrectCount' has top priority`() = runTest {
-        useCase().first().zipWithNext { a, b ->
+        val assistantCards = useCase().invoke().first()
+        assertTrue(assistantCards.size == 30)
+        assistantCards.zipWithNext { a, b ->
             assertTrue(
                 a.flashcard.additionalInfo.consecutiveCorrectCount <=
                     b.flashcard.additionalInfo.consecutiveCorrectCount,
@@ -21,7 +28,9 @@ class GetDueFlashcardsTest {
 
     @Test
     fun `Cards with older 'lastReviewAt' has top priority`() = runTest {
-        useCase().first()
+        val assistantCards = useCase().invoke().first()
+        assertTrue(assistantCards.size == 30)
+        assistantCards
             .groupBy { it.flashcard.additionalInfo.consecutiveCorrectCount }
             .forEach { (_, assistantCards) ->
                 assistantCards.zipWithNext { a, b ->
@@ -32,16 +41,62 @@ class GetDueFlashcardsTest {
 
     @Test
     fun `Cards with due 'nextReviewAt' has top priority`() = runTest {
+        val time = Clock.System.now().plus(1.hours)
+        useCase(time).invoke().first().forEach { card ->
+            assertTrue(card.flashcard.nextReviewAt <= time)
+        }
+    }
+
+    @Test
+    fun `Cards with future 'nextReviewAt' are not retrieved`() = runTest {
+        val time = Clock.System.now().minus(1.hours)
+        assertTrue(useCase(time).invoke().first().isEmpty())
     }
 
     @Test
     fun `30 Cards are returned per review`() = runTest {
-        assertTrue(useCase().first().size == 30)
+        assertTrue(useCase().invoke().first().size == 30)
     }
 
-    private val useCase
-        get() = GetDueFlashcards(
-            flashcardsRepository = FakeFlashcardsRepository(),
-            ioDispatcher = Dispatchers.Unconfined,
-        )
+    @Test
+    fun `Due cards change after review patches`() = runTest {
+        val flashcardsRepository = TestFlashcardsRepository()
+        while (useCase(repository = flashcardsRepository).invoke().first().isNotEmpty()) {
+            val patch = useCase(repository = flashcardsRepository).invoke().first()
+            assertTrue(patch.size <= 30)
+            patch.forEach { card ->
+                flashcardsRepository.updateCard(
+                    card.flashcard.copy(
+                        lastReviewAt = Clock.System.now().plus(1.hours),
+                        additionalInfo = card.flashcard.additionalInfo.copy(
+                            consecutiveCorrectCount =
+                            card.flashcard.additionalInfo.consecutiveCorrectCount + 1,
+                        ),
+                        nextReviewAt = Clock.System.now().plus(10.hours),
+                    ),
+                )
+            }
+
+            val nextPatch = useCase(repository = flashcardsRepository).invoke().first()
+            assertTrue(nextPatch.size <= 30)
+            assertNotEquals(patch, nextPatch)
+            if (nextPatch.isNotEmpty()) {
+                assertTrue(
+                    patch.last().flashcard.additionalInfo.consecutiveCorrectCount <=
+                        nextPatch.first().flashcard.additionalInfo.consecutiveCorrectCount,
+                )
+            }
+        }
+    }
+
+    private fun useCase(
+        time: Instant = Clock.System.now().plus(1.hours),
+        repository: TestFlashcardsRepository = TestFlashcardsRepository(),
+    ) = GetDueFlashcards(
+        appTime = object : AppTime {
+            override val now: Instant get() = time
+        },
+        flashcardsRepository = repository,
+        ioDispatcher = Dispatchers.Unconfined,
+    )
 }
