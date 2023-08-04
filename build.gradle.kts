500e98bd6cb895f63a0a874d2720c191adbe74ca
Mohamed <Mohamed.Metwalli@sap.com>
2023-08-04 23:53:54 +0200

    KTIJ-19369 fixed in 8.1 plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
