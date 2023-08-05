05799aa2821f015155dfcbb93a9870fa3802b2fb
Mohamed <Mohamed.Metwalli@sap.com>
2023-08-05 12:18:27 +0200

    Refactor

--- feature/assistant/src/main/java/com/memorati/feature/assistant/AssistantPager.kt
+++ feature/assistant/src/main/java/com/memorati/feature/assistant/AssistantPager.kt
@@ -2,7 +2,6 @@ package com.memorati.feature.assistant
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -24,11 +22,9 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import com.memorati.core.design.component.FavouriteButton
 import com.memorati.core.model.AssistantCard
 import com.memorati.core.model.Flashcard
 import com.memorati.core.ui.DevicePreviews
-import com.memorati.core.ui.provider.AssistantCardProvider
 import com.memorati.core.ui.provider.AssistantCardsProvider
 import com.memorati.core.ui.theme.MemoratiTheme
 import kotlinx.coroutines.launch
@@ -45,7 +41,7 @@ fun AssistantPager(
     val pagerState = rememberPagerState { assistantCards.size }
     val coroutineScope = rememberCoroutineScope()
     Column(modifier = modifier) {
-        ProgressTitle(
+        ReviewTitle(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(top = 10.dp),
@@ -101,7 +97,7 @@ private fun ReviewProgress(
 }
 
 @Composable
-private fun ProgressTitle(
+private fun ReviewTitle(
     modifier: Modifier = Modifier,
     currentPage: Int,
     count: Int,
@@ -121,60 +117,6 @@ private fun ProgressTitle(
 }
 
 @Composable
-fun AssistantPage(
-    modifier: Modifier = Modifier,
-    card: AssistantCard,
-    onNext: () -> Unit,
-    toggleFavoured: (Flashcard, Boolean) -> Unit,
-    onAnswerSelected: (AssistantCard, String) -> Unit,
-) {
-    Box(
-        modifier = modifier.padding(
-            vertical = 32.dp,
-            horizontal = 16.dp,
-        ),
-    ) {
-        FlippableCard(
-            front = {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    text = card.flashcard.idiom,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-            },
-            back = {
-                AnswerRadioButtons(
-                    card = card,
-                    onAnswerSelected = onAnswerSelected,
-                )
-            },
-        )
-
-        FavouriteButton(
-            modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.BottomStart),
-            favoured = card.favoured,
-            onCheckedChange = {
-                toggleFavoured(card.flashcard, it)
-            },
-        )
-
-        Button(
-            modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.BottomEnd),
-            enabled = card.isAnswered,
-            onClick = onNext,
-        ) {
-            Text(
-                text = stringResource(R.string.next),
-            )
-        }
-    }
-}
-
-@Composable
 @DevicePreviews
 private fun AssistantScreenPreview(
     @PreviewParameter(AssistantCardsProvider::class) assistantCards: List<AssistantCard>,
@@ -184,21 +126,6 @@ private fun AssistantScreenPreview(
             assistantCards = assistantCards,
             toggleFavoured = { _, _ -> },
             onUpdateCard = { _, _ -> },
-            onAnswerSelected = { _, _ -> },
-        )
-    }
-}
-
-@Composable
-@DevicePreviews
-private fun AssistantItemPreview(
-    @PreviewParameter(AssistantCardProvider::class) assistantCard: AssistantCard,
-) {
-    MemoratiTheme {
-        AssistantPage(
-            card = assistantCard.copy(answer = "Communication skills"),
-            onNext = {},
-            toggleFavoured = { _, _ -> },
             onAnswerSelected = { _, _ -> },
         )
     }
--- feature/assistant/src/main/java/com/memorati/feature/assistant/FlippableCard.kt
+++ feature/assistant/src/main/java/com/memorati/feature/assistant/FlippableCard.kt
@@ -6,14 +6,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
@@ -25,43 +27,36 @@ internal fun FlippableCard(
     back: @Composable BoxScope.() -> Unit,
 ) {
     var flip by remember { mutableStateOf(false) }
-    val progress = animateFloatAsState(
+    val rotation = animateFloatAsState(
         targetValue = if (flip) 180.0f else 0.0f,
         label = "FloatAsState",
         animationSpec = tween(durationMillis = 5_00),
     )
 
     Card(
-        modifier
-            .fillMaxSize()
+        modifier = modifier
             .graphicsLayer {
-                rotationX = progress.value
+                rotationX = rotation.value
                 cameraDistance = 20 * density
             }
             .heightIn(min = 250.dp),
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surfaceVariant),
     ) {
-        if (progress.value <= 90.0f) {
-            Box(
+        when {
+            rotation.value <= 90.0f -> Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .clickable {
-                        flip = !flip
-                    },
-            ) {
-                front()
-            }
-        } else {
-            Box(
+                    .clickable { flip = !flip },
+                content = front
+            )
+
+            else -> Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .graphicsLayer { rotationX = 180f }
-                    .clickable {
-                        flip = !flip
-                    },
-                contentAlignment = Alignment.Center,
-            ) {
-                back()
-            }
+                    .clickable { flip = !flip },
+                content = back
+            )
         }
     }
 }
