49a50028bd02f9def3f20fde6863a52e400f4929
Mohamed <Mohamed.Metwalli@sap.com>
2023-08-10 13:04:04 +0200

    Tweak text inputs

--- feature/creation/src/main/java/com/memorati/feature/creation/CardCreationRoute.kt
+++ feature/creation/src/main/java/com/memorati/feature/creation/CardCreationRoute.kt
@@ -11,19 +11,21 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -114,7 +116,7 @@ internal fun CardCreationScreen(
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            OutlinedTextField(
+            TextField(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
                     .fillMaxWidth(),
@@ -123,6 +125,12 @@ internal fun CardCreationScreen(
                     Text(text = stringResource(id = R.string.meaning))
                 },
                 onValueChange = onDescriptionChange,
+                shape = MaterialTheme.shapes.medium,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent,
+                )
             )
 
             Spacer(modifier = Modifier.height(20.dp))
