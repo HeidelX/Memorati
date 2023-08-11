244c03fee5c5974eea628dfae8a61acfac51f47c
Mohamed <Mohamed.Metwalli@sap.com>
2023-08-11 12:35:21 +0200

    Previews

--- feature/quiz/src/main/java/com/memorati/feature/quiz/matching/MatchingRoute.kt
+++ feature/quiz/src/main/java/com/memorati/feature/quiz/matching/MatchingRoute.kt
@@ -14,18 +14,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.memorati.core.model.Flashcard
+import com.memorati.core.ui.DevicePreviews
 import com.memorati.core.ui.provider.FlashcardsProvider
+import com.memorati.core.ui.theme.MemoratiTheme
 
 @Composable
 fun MatchingRoute(
@@ -100,12 +102,16 @@ private fun MatchingCard(
     }
 }
 
-@Preview
+@DevicePreviews
 @Composable
 private fun MatchingScreenPreview(
     @PreviewParameter(FlashcardsProvider::class) flashcards: List<Flashcard>,
 ) {
-    MatchingScreen(
-        pairs = flashcards.zip(flashcards.shuffled()),
-    )
+    MemoratiTheme {
+        Surface {
+            MatchingScreen(
+                pairs = flashcards.zip(flashcards.shuffled()),
+            )
+        }
+    }
 }
