d8de632e9a7ab731e9412bdf3ca4109902e2c399
Mohamed <mi.metwalli@gmail.com>
2024-08-22 10:12:25 +0200

    lint

--- app/src/main/java/com/memorati/MainActivity.kt
+++ app/src/main/java/com/memorati/MainActivity.kt
@@ -66,7 +66,7 @@ class MainActivity : ComponentActivity() {
                                 navController.navigateToSettings()
                             },
 
-                            )
+                        )
                         cardCreationScreen {
                             navController.navigateUp()
                         }
